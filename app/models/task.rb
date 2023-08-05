3e3093c61b4f04527a4468b79113df4aad356a3a
seaseamalon <86824027+seaseamalon@users.noreply.github.com>
2023-08-05 22:31:08 +0900

    add task model test

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+
+  validates :title, presence: true, length: {maximum: 20}
+  validates :description, presence: false, length: {maximum: 50}
+  validates :completed, inclusion: { in: [true, false] }
+  
 end
