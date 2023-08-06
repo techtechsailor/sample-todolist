07aaa79342388c30264d3232896197d27b049db1
seaseamalon <86824027+seaseamalon@users.noreply.github.com>
2023-08-06 22:58:25 +0900

    add new task page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,15 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_url
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def task_params
