6c08b4ee7ef5c06b124f1c22c228f668e8092151
techtechsailor <86824027+techtechsailor@users.noreply.github.com>
2023-08-22 07:13:57 +0900

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "tasks#index"
   get "/add", to: "tasks#new"
   resources :tasks
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,5 +1,9 @@
 require "test_helper"
 
+def setup
+  @task = tasks(:buy_eggs)
+end
+
 class TasksControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get add_path
